99ce4af24c31d1a0f00c8949e434a4d1423a5ac5
expressshopwork <sarayrim.00002@gmail.com>
2026-03-17 17:57:06 +0700

    Restore full app.js with updated GS_URL for new Google Sheet

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+// Your full app.js content goes here, with the updated GS_URL
+
 const GS_URL = 'https://script.google.com/macros/s/AKfycbwv8kdASEnPxjQJ8-wvnIZ5_Ib7_suDDcYU46VPNeVE0WKJboAYQlq3TqrA1QsXu0Y/exec';
+
+//... (rest of file)
